80857286f6e94e252dd1f33c8576cf0c04b46280
Ran-n <ran-n@tutanota.com>
2022-07-26 17:49:35 +0200

    ground_control and main separated

--- main.py
+++ main.py
@@ -1,42 +1,12 @@
 from astronaut import Astronaut
 from rocket import Rocket
-
-class GroundControl():
-
-    planets = [
-        ["moon", 24*3, "moon"],
-        ["mars", 24*30*21, "planet"],
-        ["pluto", 360*24*9.5, "planet?"]
-    ]
-
-    @staticmethod
-    def mission(rocket, astronauts, destination):
-        if len(rocket.get_crew()) == 0:
-            result, work = False, 0
-        elif rocket.get_fuel() == 9:
-            results, work = False, 0
-        elif destination[0] != GroundControl.planets[0][0] and destination[0] != GroundControl.planets[1][0] and destination[0] != GroundControl.planets[2][0]:
-            result, work = False, 0
-        else:
-            result, work = rocket.travel(destination)
-
-        def report(rocket, astronauts, result):
-            return f"The travel has {'succeeded' if result else 'failed'}.\n{len(astronauts)} crew members.\nRocket has {rocket.get_fuel()} fuel left.\nCrew has worked {work} units."
-
-        return result, work, report(rocket, astronauts, result)
-
-
-
-rocket = Rocket()
-rocket.set_name("Soyuz")
-rocket.set_fuel(1000)
-rocket.set_food(50)
+from ground_control import GroundControl
 
 astronauts = []
 for i in range(3):
     astronauts.append(Astronaut("bob"+str(i), i))
 
-rocket.set_crew(astronauts)
+rocket = Rocket("Soyuz", 1000, 50, astronauts)
 
 _, _, report = GroundControl.mission(rocket, astronauts, GroundControl.planets[0])
 print(report)
